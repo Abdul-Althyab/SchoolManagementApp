e472f842326d69ae146c1fbf893201947710787d
Abdul-Althyab <abdul.althyab@gmail.com>
2025-02-09 11:52:39 +0100

    Add data annotations to Student.cs

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,10 +1,19 @@
-﻿namespace SchoolManagementApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SchoolManagementApp.Models
 {
     public class Student
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int StudentId { get; set; }
+
+        [MinLength(3)]
+        [MaxLength(50)]
         public string StudentName { get; set; }
         public bool IsActive { get; set; }
+        [Range(5, 18)]
         public int StudentAge { get; set; }
 
     }
