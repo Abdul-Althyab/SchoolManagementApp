ab12ba70a6471a187f513310329137a2d4bce7e0
Abdul-Althyab <abdul.althyab@gmail.com>
2025-02-09 12:11:26 +0100

    Add Room.cs and data annotations to its properties

---
+++ Models/Room.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SchoolManagementApp.Models
+{
+    public class Room
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RoomId { get; set; }
+        [MinLength(3)]
+        [MaxLength(10)]
+        public string? RoomName { get; set; }
+        [Range(0, 25)]
+        public int RoomCapacity { get; set; }
+    }
+}
