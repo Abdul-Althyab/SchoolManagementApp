38089bbd0e324c5ca8945e66237a6de6df4dd109
Abdul-Althyab <abdul.althyab@gmail.com>
2025-02-09 12:54:30 +0100

    Setup MyDbContext
    Add MyDbContext and update appsettings.json
    
    Introduce MyDbContext class in SchoolManagementApp.Context namespace, inheriting from DbContext. Configure DbSets for Students, Teachers, StudentCourses, and Rooms. Override OnConfiguring to set up the database connection using a connection string from appsettings.json. Update appsettings.json to include MyConnection string for SQL Server.

---
+++ Context/MyDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolManagementApp.Models;
+
+namespace SchoolManagementApp.Context
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            base.OnConfiguring(dbContextOptionsBuilder);
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot configuration = builder.Build();
+            var connectionString = configuration.GetConnectionString("MyConnection");
+            dbContextOptionsBuilder.UseSqlServer(connectionString);
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
