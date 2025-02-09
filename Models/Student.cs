57f4278128205335eda58407934985fea20edc7c
student6390 <student6390@OVWB-7Y56GH2>
2025-02-09 11:36:07 +0100

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace SchoolManagementApp.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public bool IsActive { get; set; }
+        public int StudentAge { get; set; }
+
+    }
+}
