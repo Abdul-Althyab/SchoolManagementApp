98337855760841638588182008c7f0c3ee39cd7c
Abdul-Althyab <abdul.althyab@gmail.com>
2025-02-09 12:06:38 +0100

    Add Teacher.cs and data annotations to its properties

---
+++ Models/Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SchoolManagementApp.Models
+{
+    public class Teacher
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TeacherId { get; set; }
+        [MinLength(3)]
+        [MaxLength(50)]
+        public string? TeacherName { get; set; }
+        [Range(25, 60)]
+        public int TeacherAge { get; set; }
+    }
+}
