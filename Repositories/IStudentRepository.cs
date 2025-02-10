0566dd60e778ee969767bc227e5a8c1c878d6491
Abdul-Althyab <abdul.althyab@gmail.com>
2025-02-10 12:15:34 +0100

    Add IStudentRepository and implement StudentRepository
    Introduce IStudentRepository interface with methods for student
    management. Implement StudentRepository class using MyDbContext
    to handle database operations: GetAllStudents, Create, Delete,
    and Register. Include exception handling for all methods.

---
+++ Repositories/IStudentRepository.cs
@@ -0,0 +1,12 @@
+﻿using SchoolManagementApp.Models;
+
+namespace SchoolManagementApp.Repositories
+{
+    public interface IStudentRepository
+    {
+        public List<Student> GetAllStudents();
+        public void Create(Student student);
+        public void Delete(int studentId);
+        public void Register(int studentId, int courseId);
+    }
+}
---
+++ Repositories/StudentRepository.cs
@@ -0,0 +1,73 @@
+﻿using SchoolManagementApp.Context;
+using SchoolManagementApp.Models;
+
+namespace SchoolManagementApp.Repositories
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private readonly MyDbContext _myDbConnection;
+        public StudentRepository(MyDbContext myDbContext)
+        {
+            _myDbConnection = myDbContext;
+        }
+
+        public List<Student> GetAllStudents()
+        {
+            try
+            {
+                List<Student> students = _myDbConnection.Students.ToList();
+                return students;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+        public void Create(Student student)
+        {
+            try
+            {
+                _myDbConnection.Students.Add(student);
+                _myDbConnection.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public void Delete(int studentId)
+        {
+            try
+            {
+                _myDbConnection.Students.Remove(_myDbConnection.Students.Find(studentId));
+                _myDbConnection.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+
+
+        public void Register(int studentId, int courseId)
+        {
+            try
+            {
+                _myDbConnection.StudentCourses.Add(new StudentCourse
+                {
+                    StudentId = studentId,
+                    CourseId = courseId
+                });
+                _myDbConnection.SaveChanges();
+
+            }
+
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
