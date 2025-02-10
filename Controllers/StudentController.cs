00e5d9c1e59e491c13b25fdbfd3b0aee6e3002e7
Abdul-Althyab <abdul.althyab@gmail.com>
2025-02-10 12:33:22 +0100

    Refactor StudentController to use dependency injection
    Updated StudentController to use dependency injection for IStudentRepository:
    - Added a private readonly field `_studentRepository`.
    - Added a constructor to initialize `_studentRepository`.
    - Updated `Index`, `Create`, `Delete`, and `Register` methods to use `_studentRepository`.
    
    Updated Program.cs to register IStudentRepository and its implementation StudentRepository with the dependency injection container using `AddScoped`.

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,14 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SchoolManagementApp.Models;
+using SchoolManagementApp.Repositories;
 
 namespace SchoolManagementApp.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly IStudentRepository _studentRepository;
+        public StudentController(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
         //list of students
         [HttpGet]
         public IActionResult Index()
         {
+            List<Student> students = _studentRepository.GetAllStudents();
             return View();
         }
 
@@ -22,6 +29,7 @@ namespace SchoolManagementApp.Controllers
         public ActionResult Create(Student student)
         {
             //save the student to the database
+            _studentRepository.Create(student);
             return View();
         }
 
@@ -29,6 +37,7 @@ namespace SchoolManagementApp.Controllers
         public ActionResult Delete(int studentId)
         {
             //delete the student from the database
+            _studentRepository.Delete(studentId);
             return View();
         }
         [HttpGet]
@@ -39,6 +48,8 @@ namespace SchoolManagementApp.Controllers
         [HttpPost]
         public ActionResult Register(int studentId, int courseId)
         {
+            //register the student to the course
+            _studentRepository.Register(studentId, courseId);
             return View();
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 using SchoolManagementApp.Context;
+using SchoolManagementApp.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddSqlServer<MyDbContext>(builder.Configuration.GetConnectionString("MyConniction"));
+builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+
 
 var app = builder.Build();
 
